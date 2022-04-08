64fec952ddaf3f9bb66ef0293f30efb5eb8f6134
Alexandru M <6439106+artaeun@users.noreply.github.com>
2022-04-08 17:32:25 +0200

    Update tiroDadi.py

--- Cicli While/tiroDadi.py
+++ Cicli While/tiroDadi.py
@@ -3,7 +3,7 @@
 # l'utente deve tirare un dado 6, e inserire il valore del dado tirato
 
 # il programma deve sommare il valore del dado tirato
-# e fermarsi quando quella somma è uguale a Risultato
+# e fermarsi quando quella somma è uguale o maggiore a Risultato
 
 # il programma poi deve dire all'utente quanti tiri
 # di dado ha dobuto fare, per raggiungere il risultato

e4a0786f62c07dd66e95f8c68c8d24b533a961e2
Alexandru M <6439106+artaeun@users.noreply.github.com>
2022-04-13 16:07:48 +0200

    Create problemaDataPlan.py

---
+++ Liste/problemaDataPlan.py
@@ -0,0 +1,14 @@
+'''
+Pietro ha un piano di dati sul telefono che gli offre una quantità X di megabyte di dati per mese.
+
+In aggiunta tutti i dati che non usa in un mese vengono trasportati al mese successivo.
+
+Per esempio: se X=10mb e Pietro ne usa solo 4mb in quel mese, gli rimangono 6mb che si
+trasportano al prossimo mese. Il prossimo mese quindi ne avrà a disposizone 10+6=16mb
+
+Ci vengono il numero di mb che Pietro usa ogni N mesi. Il nostro scopo è quello di determinare quanti MB avrà il mese prossimo.
+
+x <= dato dall'utente
+n <= dato dall'utente == mesi di dati di cui sappiamo l'uso
+
+'''

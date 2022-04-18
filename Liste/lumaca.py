ebff66af372246e39c70a366128ff058765a5586
Alexandru M <6439106+artaeun@users.noreply.github.com>
2022-04-18 14:58:12 +0200

    aggiunto e corretto esercizio lumaca

---
+++ Liste/lumaca.py
@@ -0,0 +1,12 @@
+
+lunghezzaPozzo=4
+giorni=1
+
+print()
+if lunghezzaPozzo<=5:
+    print("Ci mette un giorno")
+else:
+    for i in range(lunghezzaPozzo-5):
+        giorni=giorni+1
+    print("Ci mette",giorni,"giorni.")
+print()
---
+++ Liste/test.py
@@ -0,0 +1,12 @@
+lunghezza=6
+posizioneIniziale=0
+
+giorni=0
+
+for i in range(0, lunghezza-3):
+    giorni=giorni+1
+
+if lunghezza<=5:
+    print('1')
+else:
+    print(giorni)
